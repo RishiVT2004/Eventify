87a75a0c11ad229b6f4da446051a853e85fabb84
rishi <rishivt24@gmail.com>
2024-09-22 17:41:45 +0530

    Add some extra admin routes

--- routes/AdminRoute.js
+++ routes/AdminRoute.js
@@ -5,6 +5,20 @@ import { adminSignup } from '../controller/AdminController.js';
 dotenv.config();
 const router = Router();
 
-router.post('/api/signup' , adminSignup);
+// login and signup
+router.post('/signup' , adminSignup);
+router.post('login' , adminLogin);
+
+// manage events 
+router.get('/events'); // get list of event 
+router.post('/events'); // post a new event 
+router.put('/events/:eventID'); // update info of an event 
+router.delete('/event/:eventID'); // delete an event 
+
+// manage users
+router.get('/:eventID/users'); // gets list of all users registered in a particular event 
+router.put('/users/:userID'); // updates role of a user (can make him admin)
+router.delete('/users/:userID');  // delete a user 
+
 
 export default router;
