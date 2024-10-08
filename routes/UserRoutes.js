826034ee6b6d55a767a420aee6d2e34da10c8a3a
rishi <rishivt24@gmail.com>
2024-10-08 17:50:29 +0530

    Add users route

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -6,8 +6,16 @@ import { jwtAuth } from "../middlewares/Auth.js";
 dotenv.config()
 const router = Router();
 
+// signup , signin and get profile 
 router.post('/signup',userSignup);
 router.post('/login',userLogin);
 router.get('/profile',jwtAuth,getUserProfile);
+router.put('/updateProfile'); // update user info 
+router.put('/updatePassword'); // update and change password 
+
+router.get('events/eventList'); // get lists of current event 
+router.post('/events/book/:eventID'); // book ticket for an event 
+router.delete('/event/cancel/:bookingID') // cancel booking for an event 
+router.get('/events'); // get list of event the user has bought ticket for 
 
 export default router;
