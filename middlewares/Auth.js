8592ff4935fb2a7851fe5934c4163faf683165a7
rishi <rishivt24@gmail.com>
2024-10-09 22:51:41 +0530

    Update and improve middleware

--- middlewares/Auth.js
+++ middlewares/Auth.js
@@ -1,16 +1,26 @@
 import jwt from 'jsonwebtoken'
 const JWT_KEY = process.env.JWT_KEY
-
+const ErrorTypes = {
+    nullToken : "No token provided/can't find token",
+    wrongFormat : "Wrong format for token provided , it must be *'Bearer <Token>'*",
+    invalidToken : "Token is expired or invalid",
+    noRole : "No role or invalid role assigned to token",
+    verificationError : "token verification failed",
+    missingKey : "Jwt key not provided "
+};
 export const jwtAuth = async(req,res,next) => {
+    if(!JWT_KEY){
+        return res.status(500).json({ error : ErrorTypes.missingKey });
+    }
     const token = req.headers['authorization'];
 
     if (!token) {
-        return res.status(401).json({ error: "No token provided" });
+        return res.status(401).json({ error:  ErrorTypes.wrongFormat});
     }
 
-    const DerivedToken = token.split(' ')[1];
+    const DerivedToken = token.split(' ')[1]; // extract token from bearer
     if (!DerivedToken) {
-        return res.status(401).json({ error: "Unable to derive provided" });
+        return res.status(401).json({ error: ErrorTypes.nullToken});
     }
 
     // veryfying token 
@@ -22,15 +32,15 @@ export const jwtAuth = async(req,res,next) => {
             }else if(decodedToken.role === "user"){
                 req.user = decodedToken
             }else{
-                return res.status(403).json({ message: "Invalid role in token" });
+                return res.status(403).json({ error : ErrorTypes.noRole });
             }
         }else{
-            return res.status(401).json({error : "unable to validate token"})
+            return res.status(403).json({error : ErrorTypes.invalidToken})
         }
         next();
     }catch(err){
         return res.status(403).json({
-            message : "invalid or expired token",
+            message : ErrorTypes.verificationError,
             error : err.message
         })
     }
