a05ed883f7509ed1c5743ee83df924138ddb37b6
rishi <rishivt24@gmail.com>
2024-10-10 12:44:03 +0530

    Add additional user rotes and functionality

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import dotenv from 'dotenv';
-import {userSignup,userLogin,getUserProfile} from '../controller/user/UserLogin_SignpuController.js'
+import {userSignup,userLogin} from '../controller/user/UserLogin_SignpuController.js'
+import { getUserProfile } from "../controller/user/UserProfileHandler.js";
 import { jwtAuth } from "../middlewares/Auth.js";
 
 dotenv.config()
@@ -9,13 +10,15 @@ const router = Router();
 // signup , signin and get profile 
 router.post('/signup',userSignup);
 router.post('/login',userLogin);
-router.get('/profile',jwtAuth,getUserProfile);
-router.put('/updateProfile'); // update user info 
-router.put('/updatePassword'); // update and change password 
+router.get('/profile/',jwtAuth,getUserProfile);
+
+router.put('/updateProfile/'); // update user info 
+router.put('/updatePassword/'); // update and change password 
 
 router.get('events/eventList'); // get lists of current event 
 router.post('/events/book/:eventID'); // book ticket for an event 
 router.delete('/event/cancel/:bookingID') // cancel booking for an event 
-router.get('/events'); // get list of event the user has bought ticket for 
+router.get('/:userID/events'); // get list of event the user has bought ticket for 
+
 
 export default router;
