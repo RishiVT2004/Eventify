156ee04668e747fbdd83d737320c4268c83a1a4d
rishi <rishivt24@gmail.com>
2024-10-21 12:54:27 +0530

    Added rate limitation

--- routes/AdminRoute.js
+++ routes/AdminRoute.js
@@ -4,31 +4,45 @@ import { adminSignup,adminLogin,getAdminProfile} from '../controller/admin/Admin
 import { userList,eventUserList,BanUser,UnbanUser,DeleteUser } from '../controller/admin/AdminController_UserHandler.js';
 import { createEvent , eventList , updateEvent , deleteEvent} from '../controller/admin/AdminController_EventHandler.js';
 import { jwtAuth } from '../middlewares/Auth.js';
+import rateLimiter from 'express-rate-limit'
 
 dotenv.config();
 const router = Router();
 
+const authLimiter = rateLimiter({
+    windowMs : 10*60*1000,
+    max : 5,
+    message : 'Too many requests from this IP, please try again later.'
+})
+
+const generalLimiter = rateLimiter({
+    windowMs : 60*60*1000,
+    max : 500,
+    message : 'Too many requests from this IP, please try again later.'
+})
+
+// login and signup
+router.post('/signup' , authLimiter ,adminSignup);
+router.post('/login' , authLimiter, adminLogin );
+
 // get admin info 
 router.get('/profile',jwtAuth,getAdminProfile)
-// login and signup
-router.post('/signup' , adminSignup);
-router.post('/login' , adminLogin);
-
 
 // manage users
-router.get('/userList',jwtAuth,userList); // gets list of all users registered in a particular event 
-router.get('/event/:eventID/userList',jwtAuth,eventUserList) // get list of registered in a particular event 
-router.put('/banUser/:userID',jwtAuth,BanUser);  // ban a user 
-router.put('/unbanUser/:userID',jwtAuth,UnbanUser) // unban a user
-router.delete('/deleteUser/:userID',jwtAuth,DeleteUser) // deletes a user permanently 
+router.get('/userList',generalLimiter,jwtAuth,userList); // gets list of all users registered in a particular event 
+router.get('/event/:eventID/userList',generalLimiter,jwtAuth,eventUserList) // get list of registered in a particular event 
+router.put('/banUser/:userID',generalLimiter,jwtAuth,BanUser);  // ban a user 
+router.put('/unbanUser/:userID',generalLimiter,jwtAuth,UnbanUser) // unban a user
+router.delete('/deleteUser/:userID',generalLimiter,jwtAuth,DeleteUser) // deletes a user permanently 
 
 // manage events 
-router.get('/event',jwtAuth,eventList); // get list of event 
-router.post('/event',jwtAuth,createEvent); // post a new event 
-router.put('/updateEvent/:eventID',jwtAuth,updateEvent); // update info of an event 
-router.delete('/deleteEvent/:eventID',jwtAuth,deleteEvent); // delete an event 
+router.get('/event',generalLimiter,jwtAuth,eventList); // get list of event 
+router.post('/event',generalLimiter,jwtAuth,createEvent); // post a new event 
+router.put('/updateEvent/:eventID',generalLimiter,jwtAuth,updateEvent); // update info of an event 
+router.delete('/deleteEvent/:eventID',generalLimiter,jwtAuth,deleteEvent); // delete an event 
+
 // to be implemented
-router.get('/event/:eventID/details', jwtAuth, getEventDetails);
-router.get('/event/:eventID/analytics', jwtAuth, getEventAnalytics);  
+//router.get('/event/:eventID/details', generalLimiter, jwtAuth, getEventDetails);
+//router.get('/event/:eventID/analytics', generalLimiter, jwtAuth, getEventAnalytics);  
 
 export default router;
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -5,35 +5,49 @@ import {userSignup,userLogin} from '../controller/user/UserLogin_SignpuControlle
 import { getUserProfile , updateUserProfile , changePassword} from "../controller/user/UserProfileHandler.js";
 import { getCurrentEvent , BookEvent , deleteBooking , getUserRegisteredEvents , getEventDetails} from "../controller/user/UserEventHandler.js";
 import { initiatePayment , confirmPayment , refundPayment , getPaymentStatus , listUserPayments} from "../controller/user/UserPaymentHandler.js";
+import rateLimiter from "express-rate-limit";
+
 dotenv.config()
 const router = Router();
 
+const authLimiter = rateLimiter({
+    windowMs : 10*60*1000,
+    max : 5,
+    message : 'Too many requests from this IP, please try again later.'
+})
+
+const generalLimiter = rateLimiter({
+    windowMs : 60*60*1000,
+    max : 500,
+    message : 'Too many requests from this IP, please try again later.'
+})
+
 // signup , signin 
-router.post('/signup',userSignup);
-router.post('/login',userLogin);
+router.post('/signup',authLimiter,userSignup);
+router.post('/login',authLimiter,userLogin);
 
 // profile handler
-router.get('/profile/',jwtAuth,getUserProfile);
-router.put('/updateProfile/',jwtAuth,updateUserProfile); // update user info 
-router.put('/updatePassword/',jwtAuth,changePassword); // update and change password 
+router.get('/profile/',generalLimiter,jwtAuth,getUserProfile);
+router.put('/updateProfile/',generalLimiter,jwtAuth,updateUserProfile); // update user info 
+router.put('/updatePassword/',generalLimiter,jwtAuth,changePassword); // update and change password 
 
 // event handler 
-router.get('/eventList',getCurrentEvent); // get lists of current event 
-router.get('/event/:eventID',jwtAuth , getCurrentEvent) // gets all necessary info about a particular event 
-router.post('/book/:eventID',jwtAuth,BookEvent); // book ticket for an event 
-router.delete('/cancel/:bookingID',jwtAuth,deleteBooking) // cancel booking for an event 
-router.get('/registeredEvents',jwtAuth,getUserRegisteredEvents); // get list of event the user has bought ticket for 
+router.get('/eventList',generalLimiter,getCurrentEvent); // get lists of current event 
+router.get('/event/:eventID',generalLimiter,jwtAuth , getCurrentEvent) // gets all necessary info about a particular event 
+router.post('/book/:eventID',generalLimiter,jwtAuth,BookEvent); // book ticket for an event 
+router.delete('/cancel/:bookingID',generalLimiter,jwtAuth,deleteBooking) // cancel booking for an event 
+router.get('/registeredEvents',generalLimiter,jwtAuth,getUserRegisteredEvents); // get list of event the user has bought ticket for 
 
 // payment handler 
 
-router.post('/payment/initiate/:bookingID' , jwtAuth , initiatePayment) // initiating payment for bookingID
-router.post('/payment/confirm/:paymentID' , jwtAuth , confirmPayment) // confirm the payment 
-router.post('/payment/refund/:bookingID' , jwtAuth , refundPayment) // refund the money and cancel booking
-router.get('/payment/status/:paymentID' , jwtAuth , getPaymentStatus) // users to check the status of their payments
-router.get('/payment/user' , jwtAuth , listUserPayments) // This route enables users to view all their payment transactions
+router.post('/payment/initiate/:bookingID' ,generalLimiter, jwtAuth , initiatePayment) // initiating payment for bookingID
+router.post('/payment/confirm/:paymentID' ,generalLimiter, jwtAuth , confirmPayment) // confirm the payment 
+router.post('/payment/refund/:bookingID' , generalLimiter,jwtAuth , refundPayment) // refund the money and cancel booking
+router.get('/payment/status/:paymentID' , generalLimiter,jwtAuth , getPaymentStatus) // users to check the status of their payments
+router.get('/payment/user' , jwtAuth , generalLimiter,listUserPayments) // This route enables users to view all their payment transactions
 
 // Event Reviews (to be implemented)
-router.post('/event/:eventID/review', jwtAuth, addEventReview); // Add a review for an event
-router.get('/event/:eventID/reviews', getEventReviews); // Fetch reviews for an event
+//router.post('/event/:eventID/review', jwtAuth, addEventReview); // Add a review for an event
+//router.get('/event/:eventID/reviews', getEventReviews); // Fetch reviews for an event
 
 export default router;
